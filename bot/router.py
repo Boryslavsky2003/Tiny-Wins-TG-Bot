c4911f30f9b3daab8069cdda22219dff6f082305
Boryslavsky2003 <temp2003late@gmail.com>
2025-05-09 18:49:52 +0300

    updated file names to more understandable ones

--- bot/router.py
+++ bot/router.py
@@ -1,12 +1,14 @@
 from aiogram import Router
 
 from bot.handlers import (
-    hd_channel_id,
+    hd_get_channel_id,
+    hd_get_user_id,
     hd_start,
 )
 
 router = Router()
 
 
-router.include_router(hd_channel_id.router)
+router.include_router(hd_get_channel_id.router)
+router.include_router(hd_get_user_id.router)
 router.include_router(hd_start.router)
