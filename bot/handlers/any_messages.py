3083753fd612d51d417fde6edc6657bcd583fb4f
Boryslavsky2003 <temp2003late@gmail.com>
2025-05-19 16:37:15 +0300

    add any_text

---
+++ bot/handlers/any_messages.py
@@ -0,0 +1,19 @@
+from aiogram import Router, F
+from aiogram.types import Message
+
+from bot.utils.access import admin_only
+
+
+router = Router()
+
+
+@router.message(F.text)
+@admin_only
+async def echo(message: Message):
+    await message.answer(
+        await message.answer(
+            f"👋 {message.from_user.first_name}, щоб почати роботу, будь ласка, введіть команду /start",
+            parse_mode="Markdown",
+        ),
+        parse_mode="Markdown",
+    )
--- bot/router.py
+++ bot/router.py
@@ -1,6 +1,9 @@
 from aiogram import Router
 
-from bot.handlers import hd_start
+from bot.handlers import (
+    hd_start,
+    any_messages,
+)
 from bot.handlers.callbacks import (
     call_create_image_ai,
     call_create_text_ai,
@@ -13,6 +16,7 @@ router = Router()
 
 
 router.include_router(hd_start.router)
+router.include_router(any_messages.router)
 
 router.include_router(call_create_image_ai.router)
 router.include_router(call_create_text_ai.router)
