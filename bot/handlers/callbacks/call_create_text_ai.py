d2890809987a49f7c77d931e5028442245ae5d6c
Boryslavsky2003 <temp2003late@gmail.com>
2025-05-19 16:24:02 +0300

    rename callback func

--- bot/handlers/callbacks/call_create_text_ai.py
+++ bot/handlers/callbacks/call_create_text_ai.py
@@ -4,9 +4,10 @@ from aiogram.fsm.context import FSMContext
 from bot.states import ChannelsState
 from bot.utils.access import admin_only
 
+
 router = Router()
 
 
 @router.callback_query(F.data == "create_text_ai")
-async def handle_get_id(callback: CallbackQuery, state: FSMContext):
+async def handle_create_image_ai(callback: CallbackQuery, state: FSMContext):
     await callback.message.answer("Test")
--- bot/handlers/callbacks/call_test_ai.py
+++ bot/handlers/callbacks/call_test_ai.py
@@ -4,10 +4,14 @@ from aiogram.fsm.context import FSMContext
 from bot.states import ChannelsState
 from bot.utils.access import admin_only
 
+from loguru import logger
+
 
 router = Router()
 
 
 @router.callback_query(F.data == "test_ai")
-async def handle_get_id(callback: CallbackQuery, state: FSMContext):
-    await callback.message.answer("Test")
+async def handle_create_image_ai(callback: CallbackQuery, state: FSMContext):
+    logger.debug(f"Received callback: {callback.data}")
+    await callback.answer()
+    await callback.message.answer("Test received!")
